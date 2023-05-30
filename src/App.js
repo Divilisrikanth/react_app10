0078916df7b712e0aff80ce8c7f4bc4038a97818
Divilisrikanth <srikanthdivili1994@gmail.com>
2023-05-30 19:58:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+   const [data,setData] = useState(null)
+   const [print,setPrint]= useState(false)
+  function getData(val)
+   {
+    setData(val.target.value)
+    console.warn(val.target.value)
+   }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        print?
+        <h1>{data}</h1>
+        :null
+      }
+      <input type="text" onChange={getData} />
+      <button onClick={()=>setPrint(true)} >Print Value</button>
     </div>
   );
 }
